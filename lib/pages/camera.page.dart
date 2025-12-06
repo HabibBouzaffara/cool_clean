3ba35debd8bee6a97b47708a5a74f3cb15433700
mohamedba26 <122885497+mohamedba26@users.noreply.github.com>
2025-12-06 17:17:06 +0100

    First commit

---
+++ lib/pages/camera.page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class Camera extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+}
